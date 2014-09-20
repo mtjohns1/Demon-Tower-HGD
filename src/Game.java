77bea01f987f94c5e189c614c8a699c139dec1b5
Thomas Black <twblack@mtu.edu>
2014-09-20 16:56:57 -0400

    Running Screen will bring up a dummy game.

--- src/Game.java
+++ src/Game.java
@@ -7,14 +7,61 @@
 import javax.swing.*;
 import javax.swing.event.*;
 import javax.imageio.ImageIO;
+
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.*;
 
-public class Game {
+public class Game extends JFrame implements ActionListener, KeyListener {
+	
+	
+	public JPanel box = new JPanel(new FlowLayout());
+	
+	//private JButton exit = new JButton("Exit Button");
+	
+	
+	public Game(){
+		super("This is a game placeholder");
+		//box.add(exit);
+		final int WIDTH = 640;
+		final int HEIGHT = 480; 
+		this.add(box);
+		
+		setLocationByPlatform(true);
+		this.setSize(WIDTH, HEIGHT);
+		this.setResizable(false);
 
-	public static void main(String[] args) {
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+	}
+	
+	
+	@Override
+	public void keyPressed(KeyEvent arg0) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
 }
--- src/Screen.java
+++ src/Screen.java
@@ -7,9 +7,26 @@
 import javax.swing.*;
 import javax.swing.event.*;
 import javax.imageio.ImageIO;
+
 import java.awt.*;
 import java.io.*;
 
 public class Screen {
+	
+	
+	public static void startup(){
+		final Game game = new Game();
+		game.setVisible(true);
+    }
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		startup();
+	}
 
+
+	
+	
 }
