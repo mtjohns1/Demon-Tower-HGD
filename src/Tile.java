89a9ee4285f52cf8a20c5e7ab70544071be0c048
jecharle <jecharle@mtu.edu>
2014-10-12 14:22:10 -0400

    Finished out Tile functionality, except .draw().

--- src/Tile.java
+++ src/Tile.java
@@ -1,27 +1,82 @@
 /**
  * 
  * A class for individual tiles
- *
+ * @author Jacob Charles
+ * @author Brett Miller
  */
 
 import javax.swing.*;
 import javax.swing.event.*;
 import javax.imageio.ImageIO;
+
 import java.awt.*;
 import java.io.*;
 
 public class Tile {
 
-	private String tile_type;
+	private Room _home;
+	private int _x, _y;
+	private String _tile_type;
 	
-	public String getType()
-	{
-		return tile_type;
+	/**
+	 * Initialize a tile within a room
+	 * 
+	 * @param r the room it's part of
+	 * @param x the horizontal location in the map
+	 * @param y the vertical location in the map
+	 */
+	public Tile(Room r, int x, int y) {
+		_home = r;
+		_x = x;
+		_y = y;
 	}
 	
-	public void setType(String x)
+	/**
+	 * @return y coordinate of the top edge
+	 */
+	public int getTop() {
+		return _y*32;
+	}
+	/**
+	 * @return y coordinate of the bottom edge
+	 */
+	public int getBottom() {
+		return _y*32+32;
+	}
+	/**
+	 * @return x coordinate of the left edge
+	 */
+	public int getLeft() {
+		return _x*32;
+	}
+	/**
+	 * @return x coordinate of the right edge
+	 */
+	public int getRight() {
+		return _x*32+32;
+	}
+	
+	/**
+	 * @return the room the tile is part of
+	 */
+	public Room getHome() {
+		return _home;
+	}
+	
+	/**
+	 * @return the tile's type (string format)
+	 */
+	public String getType()
 	{
-		this.tile_type = x;
+		return _tile_type;
+	}
+	
+	/**
+	 * @param s the new tile type
+	 */
+	public void setType(String s)
+	{
+		this._tile_type = s;
 	}
 	
 }
